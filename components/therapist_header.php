6bdec1a456e6d0bbab4bcd847262eba9c631c091
alaaomaribrahim <118222820+alaaomaribrahim@users.noreply.github.com>
2024-05-22 03:22:21 +0300

    components folder

---
+++ components/therapist_header.php
@@ -0,0 +1,100 @@
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+
+<header class="header">
+
+   <section class="flex">
+
+      <a href="therapistdash.php" class="logo">Therapist.</a>
+
+      
+
+      <div class="icons">
+         <div id="menu-btn" class="fas fa-bars"></div>
+         <div id="user-btn" class="fas fa-user"></div>
+         <div id="toggle-btn" class="fas fa-sun"></div>
+      </div>
+
+      <div class="profile">
+         <?php
+            $select_profile = $conn->prepare("SELECT * FROM `therapist` WHERE id = ?");
+            $select_profile->execute([$therapist_id]);
+            if($select_profile->rowCount() > 0){
+            $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+         ?>
+         <img src="../uploaded_files/<?= $fetch_profile['image']; ?>" alt="">
+         <h3><?= $fetch_profile['name']; ?></h3>
+         <span><?= $fetch_profile['profession']; ?></span>
+         <a href="profile.php" class="btn">view profile</a>
+         <div class="flex-btn">
+            <a href="therapistlogin.php" class="option-btn">login</a>
+         </div>
+         <a href="../components/admin_logout.php" onclick="return confirm('logout from this website?');" class="delete-btn">logout</a>
+         <?php
+            }else{
+         ?>
+         <h3>please login or register</h3>
+          <div class="flex-btn">
+            <a href="login.php" class="option-btn">login</a>
+            <a href="register.php" class="option-btn">register</a>
+         </div>
+         <?php
+            }
+         ?>
+      </div>
+
+   </section>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- side bar section starts  -->
+
+<div class="side-bar">
+
+   <div class="close-side-bar">
+      <i class="fas fa-times"></i>
+   </div>
+
+   <div class="profile">
+   <?php
+            $select_profile = $conn->prepare("SELECT * FROM `therapist` WHERE id = ?");
+            $select_profile->execute([$therapist_id]);
+            if($select_profile->rowCount() > 0){
+            $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+         ?>
+         <img src="../uploaded_files/<?= $fetch_profile['image']; ?>" alt="">
+         <h3><?= $fetch_profile['name']; ?></h3>
+         <span><?= $fetch_profile['profession']; ?></span>
+        <!--  <a href="profile.php" class="btn">view profile</a> -->
+         
+         <?php
+            }else{
+         ?>
+         
+         <?php
+            }
+         ?>
+      </div>
+
+   <nav class="navbar">
+      <a href="therapistdash.php"><i class="fas fa-home"></i><span>home</span></a>
+      <a href="user_grades.php"><i class="fas fa-graduation-cap"></i><span>kids grades</span></a>
+      <a href="therapist.php"><i class="fas fa-graduation-cap"></i><span>Appointment Schedule</span></a>
+      <a href="../components/admin_logout.php" onclick="return confirm('logout from this website?');"><i class="fas fa-right-from-bracket"></i><span>logout</span></a>
+   </nav>
+
+</div>
+
+<!-- side bar section ends -->
